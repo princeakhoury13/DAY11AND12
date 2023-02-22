6d6abb80a13b8c72b0c8ae133d40073e67a585e5
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-22 15:17:59 +0530

    added: Q1 and Q2: Inventory management for Rice, Pulses and Wheat and their price

---
+++ FetchInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class FetchInventory
+    {
+        public InventoryDetails Read(string path)
+        {
+            using(StreamReader file= new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+                }
+                catch(Exception ex) { return null; }
+            }
+        }
+    }
+}
---
+++ InventoryDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfPulses> typesOfPulses;
+        public List<TypesOfWheat> typesOfWheat;
+    }
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
